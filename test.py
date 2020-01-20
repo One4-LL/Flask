64fb1c3963e26632dd6ab31268b23276963787b6
Ujwalkpl <ujwlkpl@gmail.com>
2020-01-20 09:00:41 +0530

    added ML

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
 from googletrans import Translator
-import time
 from firebase_admin import credentials, firestore, initialize_app
 import json
 from flask import Flask, request,session #import main Flask class and request object
 import requests
-import pandas as pd
 app = Flask(__name__) #create the Flask app
 app.secret_key = "Revhack"
 texts = {}
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 
 from nltk.stem import PorterStemmer
 ps =PorterStemmer()
@@ -40,24 +36,9 @@ def hello_world():
 @app.route('/query-example')
 
 def query_example():
-   
-    def translatekaro(stringd,lang):
-        api_url = 'https://hackapi.reverieinc.com/nmt'
-        create_row_data = {'data':[stringd],
-        'tgt':lang,
-        'src':'en'}
-        print("hi"+str(create_row_data))
-        r = requests.post(url=api_url,headers={'Content-Type':'application/json','token' : '35352f14ab2403b1b1ca50bb0a65d9930a9a98ba'}, json=create_row_data)
-        listemp = json.loads(r.text)['data']['result'][0][0]
-        print(listemp)
-        return listemp 
-        
-
+    
     docs = db.collection(u'Orders').get()
     session.clear()
-    list1 = []
-    list2 = []
-    list3 = []
     for doc in docs:
         # print(u'{} => {}'.format(doc.id, doc.to_dict()))
         
@@ -69,98 +50,64 @@ def query_example():
         
         dummy = {}
         dummyvalue = 0
-        
-       
+
         for key,value in texts.items():
             
-            if "kg" in value.lower():
+            if "kg" in value:
                 dummyvalue = float(value.split()[0])
-            if "gram" in value.lower():
+            if "grams" in value:
                 dummyvalue = round(float(value.split()[0])/1000,2)
                 texts[key] = value
-            elif "pound" in value.lower():
+            elif "pounds" in value:
                 dummyvalue = round(float(value.split()[0])/2.205,2)
                 texts[key] = value
-            
-            key= ps.stem(key.lower())
-                
-             
+            key= ps.stem(key)
             dummy[key] = dummyvalue
             
         texts = dummy
         print(texts)
-        database = {'sugar':30,'rice':20,'kurkure':5,'tomato':39,'onion':100,'wheat':40,'potato':15,'appl':110,'banana':40,'mango':90,'orange':40,'orang':40,'bananas':40}
-        remaining = {'sugar':10,'rice':8,'kurkure':10,'tomato':7,'onion':9,'wheat':13,'potato':10,'appl':10,'banana':7,'mango':5,'orange':8,'orang':6,'bananas':8}
-        
-        try:
-            for key,value in texts.items():
-            
-                list1.append(texts[key])
-                list2.append(database[key])
-                list3.append(key)
-        
-        except KeyError:
-            print("not in list")
-
-        
-        
         for key,value in texts.items():
             if key in session:
                 session[key] = int(session[key])+int(texts[key])
             else:
                 session[key] = int(texts[key])
-       
-        
+        print(session)
+        print(type(session))
         translator = Translator()
         translated = {}
         userPrice = {}
+        print(texts)
+        database = {'sugar':30,'rice':20,'kurkure':5,'tomato':39,'onion':100,'wheat':40,'potato':15,'appl':110,'banana':40,'mango':90}
+        remaining = {'sugar':20,'rice':20,'kurkure':25,'tomato':32,'onion':40,'wheat':40,'potato':35,'appl':60,'banana':50,'mango':10}
         
-       
         
-        try: 
-            for key, value in texts.items():
-
-                translated[translatekaro(stringd=key,lang="hi")]=value
-                time.sleep(1.5)
-                remaining[key] = remaining[key]- texts[key]
-                time.sleep(1.5)
-                userPrice[translatekaro(stringd=key,lang="hi")] = database[key]
-        except KeyError:
-            print("not found")
+        for key, value in texts.items():
+            translated[translator.translate(key, dest='kn').text]=value
+            remaining[key] = remaining[key]- texts[key]
+        for key,value in texts.items():
+            userPrice[translator.translate(key, dest='kn').text] = database[key]
         # for key, value in  texts.items():
         #     texts[translator.translate(key, dest='en').text]=value
         
         print("last"+str(texts))
             
         cost = 0
-        try:
-
-            for key,value in texts.items():
-                cost = cost + int(texts[key])*database[key]
-
-        except KeyError:
-            print("not found")
+        for key,value in texts.items():
+            cost = cost + int(texts[key])*database[key]
+        
         
         data = {'cost':cost,'userOrder':translated,'userPrice':userPrice}
         
         tOrders.document(doc.id).set(data)
-        print()
-        print()
-    
-    data = {'item':list3,'count': list1, 'cost': list2}
-    df = pd.DataFrame.from_dict(data)
-    df.to_csv('file1.csv',index=True,index_label='Time')
-   
-    
     total = {}
     for key in session:
         total[key] = session[key]
-    stocks = {'banana': '20-01-2020', 'appl': '21-01-2020', 'sugar': '22-01-2020', 'wheat': '23-01-2020', 'rice': '21-01-2020', 'tomato': '20-01-2020', 'potato': '22-01-2020', 'mango': '19-01-2020'}    
-    data2 = {'Daily Expense':total,'remaining':remaining,'stocks':stocks}
+    data2 = {'Daily Expense':total,'remaining':remaining}
     SDetails.document("katabook").set(data2)
     return "Aise taise kara lo"
 
 
+    
 
     
 if __name__ == '__main__':
